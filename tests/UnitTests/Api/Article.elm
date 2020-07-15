d7ecea41bdb140b946f2556b67bb6d248e3dcc80
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2020-07-15 00:42:00 -0500

    indentation because it doesnt matter

--- tests/UnitTests/Api/Article.elm
+++ tests/UnitTests/Api/Article.elm
@@ -36,40 +36,43 @@ suite =
             \_ ->
                 """
                 {
-                    "articles":[{
-                        "slug": "how-to-train-your-dragon",
-                        "title": "How to train your dragon",
-                        "description": "Ever wonder how?",
-                        "body": "It takes a Jacobian",
-                        "tagList": ["dragons", "training"],
-                        "createdAt": "2016-02-18T03:22:56.637Z",
-                        "updatedAt": "2016-02-18T03:48:35.824Z",
-                        "favorited": false,
-                        "favoritesCount": 0,
-                        "author": {
-                        "username": "jake",
-                        "bio": "I work at statefarm",
-                        "image": "https://i.stack.imgur.com/xHWG8.jpg",
-                        "following": false
+                    "articles": [
+                        {
+                            "slug": "how-to-train-your-dragon",
+                            "title": "How to train your dragon",
+                            "description": "Ever wonder how?",
+                            "body": "It takes a Jacobian",
+                            "tagList": ["dragons", "training"],
+                            "createdAt": "2016-02-18T03:22:56.637Z",
+                            "updatedAt": "2016-02-18T03:48:35.824Z",
+                            "favorited": false,
+                            "favoritesCount": 0,
+                            "author": {
+                                "username": "jake",
+                                "bio": "I work at statefarm",
+                                "image": "https://i.stack.imgur.com/xHWG8.jpg",
+                                "following": false
+                            }
+                        },
+                        {
+                            "slug": "how-to-train-your-dragon-2",
+                            "title": "How to train your dragon 2",
+                            "description": "So toothless",
+                            "body": "It a dragon",
+                            "tagList": ["dragons", "training"],
+                            "createdAt": "2016-02-18T03:22:56.637Z",
+                            "updatedAt": "2016-02-18T03:48:35.824Z",
+                            "favorited": false,
+                            "favoritesCount": 0,
+                            "author": {
+                                "username": "jake",
+                                "bio": "I work at statefarm",
+                                "image": "https://i.stack.imgur.com/xHWG8.jpg",
+                                "following": false
+                            }
                         }
-                    }, {
-                        "slug": "how-to-train-your-dragon-2",
-                        "title": "How to train your dragon 2",
-                        "description": "So toothless",
-                        "body": "It a dragon",
-                        "tagList": ["dragons", "training"],
-                        "createdAt": "2016-02-18T03:22:56.637Z",
-                        "updatedAt": "2016-02-18T03:48:35.824Z",
-                        "favorited": false,
-                        "favoritesCount": 0,
-                        "author": {
-                        "username": "jake",
-                        "bio": "I work at statefarm",
-                        "image": "https://i.stack.imgur.com/xHWG8.jpg",
-                        "following": false
-                        }
-                    }],
-                "articlesCount": 2
+                    ],
+                    "articlesCount": 2
                 }
                 """
                     |> Json.decodeString
